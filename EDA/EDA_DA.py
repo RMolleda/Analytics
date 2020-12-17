cc5917da6f6b62fec586230e69bdb0a85828ee7a
RMolleda <molledajr@gmail.com>
2020-12-17 15:08:11 +0100

    Backup 17-12-20

---
+++ EDA/EDA_DA.py
@@ -0,0 +1,25 @@
+def impData():
+    import pandas as pd 
+    import requests
+    import json
+
+def impPlot():
+    import pandas as pd 
+    import matplotlib as plt
+
+def impNP():
+    import pandas as pd 
+    import numpy as np 
+    import requests 
+
+def readCSV(url, delim):
+    import pandas as pd 
+    readed = pd.read_csv(url, delimiter=delim)
+    return readed
+    import json
+
+def impScraping():
+    from bs4 import BeautifulSoup
+    import pandas as pd 
+    import selenium as sl
+
